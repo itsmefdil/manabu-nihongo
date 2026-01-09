85aefd7f8d4754ccfc8fbb782535b82ea418d763
Fadilah Riczky <friczky@gmail.com>
2026-01-09 13:32:14 +0700

    feat: fix unsupport dir input at vercel

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import routes from './routes';
+import routes from './routes/index';
 import { errorHandler } from './middleware/error';
 
 const app = express();
